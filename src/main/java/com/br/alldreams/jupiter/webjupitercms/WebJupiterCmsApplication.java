2734ec556c57aa346874a8b6f1f624b7f7dbb1cc
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2020-01-10 23:20:28 -0300

    Subindo a primeira versão
    Subindo a primeira versão

---
+++ src/main/java/com/br/alldreams/jupiter/webjupitercms/WebJupiterCmsApplication.java
@@ -0,0 +1,13 @@
+package com.br.alldreams.jupiter.webjupitercms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJupiterCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJupiterCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/alldreams/jupiter/webjupitercms/WebJupiterCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alldreams.jupiter.webjupitercms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJupiterCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
