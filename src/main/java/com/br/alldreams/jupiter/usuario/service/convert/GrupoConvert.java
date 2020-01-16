b8a34176eb6e50d8eb8fa159f626f9489b25fafb
Jorge Demetrio Sobieski Teixeira <jorge.demetrio@alldreams.com.br>
2020-01-16 11:13:09 -0300

    Removendo comentarios

--- src/main/java/com/br/alldreams/jupiter/usuario/service/convert/GrupoConvert.java
+++ src/main/java/com/br/alldreams/jupiter/usuario/service/convert/GrupoConvert.java
@@ -18,19 +18,5 @@ import com.br.alldreams.jupiter.usuario.repository.model.Grupo;
 		Grupo.class })
 public interface GrupoConvert extends ConvertPadrao<GrupoDTO, Grupo> {
 
-//
-//	GrupoDTO toGrupo(Grupo grupo);
-//
-//	@InheritInverseConfiguration
-//	Grupo toGrupo(GrupoDTO grupo);
-//
-//	@Mapping(target = "id", ignore = true)
-//	@Mapping(target = "criador", ignore = true)
-//	@Mapping(target = "dataCriacao", ignore = true)
-//	@Mapping(target = "ipCriador", ignore = true)
-//	Grupo toGrupo(GrupoDTO grupo, @MappingTarget Grupo origem);
-//
-//	List<Grupo> toGrupos(List<GrupoDTO> grupo);
-//
-//	List<GrupoDTO> toGruposDTO(List<Grupo> grupo);
+
 }
